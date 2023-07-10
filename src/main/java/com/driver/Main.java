49a0c64acd001770bf84b0496b63425b3c463a86
Tushar <tushargarg0110@gmail.com>
2023-07-10 23:21:28 +0530

    Code changes done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -18,17 +18,10 @@ public class Main {
     public static void main(String[] args) {
 
         B obj = new B();
-
         obj.meth();
 
-
         B obj1 = new B();
-
         obj1.meth();
-
-
         //Automatically call the function of class A meth
-
-
     }
 }
